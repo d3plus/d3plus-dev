964bab7cfe8ec9cdd5edff1d9814ca68f5ed7b6e
Dave Landry <landry.dave@gmail.com>
2016-10-27 10:56:28 -0400

    makes default script logo more modest

--- bin/logo.js
+++ bin/logo.js
@@ -1,23 +1,38 @@
 const chalk = require("chalk"),
       shell = require("shelljs");
 
-module.exports = function(name = "") {
+module.exports = function(name = "", mode) {
 
-  const pad = 25;
-  name = `${Array(pad).fill(" ").join("")}${name.toUpperCase()}`.slice(-pad);
+  switch (mode) {
 
-  shell.echo(`
-   ${chalk.gray("█▀▀▀▀▀▀▀▄▄      ▄▄▀▀▀▀▀▀▄▄ ")}
-   ${chalk.gray("█         ▀▄   █          █")}
-   ${chalk.gray("█           █  █          █")}  ${chalk.green("         ▄▄")}
-   ${chalk.gray("█           █       ▄▄▄▄▄▀ ")}  ${chalk.green("         ██")}
-   ${chalk.gray("█           █            ▀▄")}  ${chalk.green("██▀▀▀█▄  ██ ██   ██ ▄█▀▀▀▀▄")}
-   ${chalk.gray("█           █  █          █")}  ${chalk.green("██    ██ ██ ██   ██ ▀█▄▄▄")}
-   ${chalk.gray("█        ▄▄▀   █          █")}  ${chalk.green("██    ██ ██ ██   ██    ▀▀█▄")}
-   ${chalk.gray("█▄▄▄▄▄▄▀▀       ▀▀▄▄▄▄▄▄▀▀ ")}  ${chalk.green("██▄▄▄█▀  ██ ▀█▄▄▀██ ▀▄▄▄▄█▀")}
-                                ${chalk.green("██")}
-                                ${chalk.green("██")}${chalk.black(name)}
+    case "ascii":
 
-  `);
+      const pad = 25;
+      name = `${Array(pad).fill(" ").join("")}${name.toUpperCase()}`.slice(-pad);
+
+      shell.echo(`
+       ${chalk.dim("▄▄▄▄▄▄▄▄          ▄▄▄▄▄▄   ")}
+       ${chalk.dim("█       ▀▀▄    ▄▀▀      ▀▀▄")}
+       ${chalk.dim("█          ▀▄  █          █")}  ${chalk.green("         ▄▄")}
+       ${chalk.dim("█           █  ▀         ▄▀")}  ${chalk.green("         ██")}
+       ${chalk.dim("█           █       ▀▀▀▀▀▄ ")}  ${chalk.green("██▀▀▀█▄  ██ ██   ██ ▄█▀▀▀▀▄")}
+       ${chalk.dim("█           █  █          █")}  ${chalk.green("██    ██ ██ ██   ██ ▀█▄▄▄")}
+       ${chalk.dim("█        ▄▄▀   █          █")}  ${chalk.green("██    ██ ██ ██   ██    ▀▀█▄")}
+       ${chalk.dim("█▄▄▄▄▄▄▀▀       ▀▀▄▄▄▄▄▄▀▀ ")}  ${chalk.green("██▄▄▄█▀  ██ ▀█▄▄▀██ ▀▄▄▄▄█▀")}
+                                    ${chalk.green("██")}
+                                    ${chalk.green("██")}${chalk.black(name)}
+
+      `);
+
+      break;
+
+    default:
+      shell.echo(`
+    ${chalk.bold.black("D3")}${chalk.bold.green("plus")}
+    ${chalk.dim(name)}
+
+`);
+
+  }
 
 };
