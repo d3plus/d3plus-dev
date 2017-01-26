544e1db743356a41bc7423890d22b70884573ef5
Dave Landry <landry.dave@gmail.com>
2017-01-26 10:06:05 -0500

    creates test tests

---
+++ test/folder/nested.js
@@ -0,0 +1,8 @@
+import {test} from "tape";
+
+test("nested", assert => {
+
+  assert.equals(true, true, "nested test directory");
+  assert.end();
+
+});
---
+++ test/root.js
@@ -0,0 +1,8 @@
+import {test} from "tape";
+
+test("root", assert => {
+
+  assert.equals(true, true, "root test directory");
+  assert.end();
+
+});
