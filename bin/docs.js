b70187d260a66c91fa20326ff9827b7400847831
Dave Landry <landry.dave@gmail.com>
2017-02-08 12:56:39 -0500

    detects files in d3plus-docs script

--- bin/docs.js
+++ bin/docs.js
@@ -88,7 +88,7 @@ ${examples}
 `;
 new shell.ShellString(contents).to(template);
 
-shell.exec(`jsdoc2md '+(bin|src)/**/*.js' --heading-depth 3 -t ${template} > README.md`, (code, stdout) => {
+shell.exec(`jsdoc2md '+(bin|src)/**/*.+(js|jsx)' --heading-depth 3 -t ${template} > README.md`, (code, stdout) => {
   if (code) {
     log.fail();
     shell.echo(stdout);
