98bae02af97d58a98e8bd80799d1bb8613f9ee57
Dave Landry <landry.dave@gmail.com>
2017-04-12 17:47:59 -0400

    adds `clear` to logo script (closes #30)

--- bin/logo.js
+++ bin/logo.js
@@ -1,38 +1,14 @@
 const chalk = require("chalk"),
       shell = require("shelljs");
 
-module.exports = function(name = "", mode) {
+module.exports = function(name = "development script") {
+  shell.exec("clear");
 
-  switch (mode) {
+  shell.echo(`
 
-    case "ascii":
-
-      const pad = 25;
-      name = `${Array(pad).fill(" ").join("")}${name.toUpperCase()}`.slice(-pad);
-
-      shell.echo(`
-       ${chalk.dim("▄▄▄▄▄▄▄▄          ▄▄▄▄▄▄   ")}
-       ${chalk.dim("█       ▀▀▄    ▄▀▀      ▀▀▄")}
-       ${chalk.dim("█          ▀▄  █          █")}  ${chalk.green("         ▄▄")}
-       ${chalk.dim("█           █  ▀         ▄▀")}  ${chalk.green("         ██")}
-       ${chalk.dim("█           █       ▀▀▀▀▀▄ ")}  ${chalk.green("██▀▀▀█▄  ██ ██   ██ ▄█▀▀▀▀▄")}
-       ${chalk.dim("█           █  █          █")}  ${chalk.green("██    ██ ██ ██   ██ ▀█▄▄▄")}
-       ${chalk.dim("█        ▄▄▀   █          █")}  ${chalk.green("██    ██ ██ ██   ██    ▀▀█▄")}
-       ${chalk.dim("█▄▄▄▄▄▄▀▀       ▀▀▄▄▄▄▄▄▀▀ ")}  ${chalk.green("██▄▄▄█▀  ██ ▀█▄▄▀██ ▀▄▄▄▄█▀")}
-                                    ${chalk.green("██")}
-                                    ${chalk.green("██")}${chalk.black(name)}
-
-      `);
-
-      break;
-
-    default:
-      shell.echo(`
-    ${chalk.bold.black("D3")}${chalk.bold.green("plus")}
-    ${chalk.dim(name)}
+    ${chalk.bold.green("D3plus 2.0")}
+    ${chalk.green(name)}
 
 `);
-
-  }
 
 };
