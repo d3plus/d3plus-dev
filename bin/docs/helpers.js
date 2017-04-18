74b042ef1d705765df2d31d1d18f2f6b750acea4
Dave Landry <landry.dave@gmail.com>
2017-04-18 17:17:26 -0400

    fixes dynamic description text in body of docs

--- bin/docs/helpers.js
+++ bin/docs/helpers.js
@@ -10,3 +10,41 @@ exports.codeLink = meta => {
   folders = folders.slice(index + 1);
   return `[<>](https://github.com/d3plus/${name}/blob/master${ folders.length ? `/${folders.join("/")}` : "" }/${filename}#L${lineno})`;
 };
+
+/* a helper to execute javascript expressions
+ USAGE:
+ -- Yes you NEED to properly escape the string literals or just alternate single and double quotes
+ -- to access any global function or property you should use window.functionName() instead of just functionName(), notice how I had to use window.parseInt() instead of parseInt()
+ -- this example assumes you passed this context to your handlebars template( {name: 'Sam', age: '20' } )
+ <p>Url: {{x " \"hi\" + name + \", \" + window.location.href + \" <---- this is your href,\" + " your Age is:" + window.parseInt(this.age, 10) "}}</p>
+ OUTPUT:
+ <p>Url: hi Sam, http://example.com <---- this is your href, your Age is: 20</p>
+*/
+exports.x = expression => {
+
+  try {
+    return eval(expression);
+  }
+  catch (e) {
+    console.warn(`•Expression: {{x '${expression}'}}\n•JS-Error: ${e}\n•Context: ${this}`);
+    return undefined;
+  }
+
+};
+
+// for detailed comments and demo, see my SO answer here http://stackoverflow.com/questions/8853396/logical-operator-in-a-handlebars-js-if-conditional/21915381#21915381
+
+/* a helper to execute an IF statement with any expression
+  USAGE:
+ -- Yes you NEED to properly escape the string literals, or just alternate single and double quotes
+ -- to access any global function or property you should use window.functionName() instead of just functionName()
+ -- this example assumes you passed this context to your handlebars template( {name: 'Sam', age: '20' } ), notice age is a string, just for so I can demo parseInt later
+ <p>
+   {{#xif " name == 'Sam' && age === '12' " }}
+     BOOM
+   {{else}}
+     BAMM
+   {{/xif}}
+ </p>
+ */
+exports.xif =  (expression, options) => exports.x.apply(this, [expression, options]) ? options.fn(this) : options.inverse(this);
