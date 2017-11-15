760d4f0670616c60e07f0006028df4e825acdd6a
Dave Landry <landry.dave@gmail.com>
2017-11-15 16:14:37 -0500

    updates rollup (closes #49)

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+
+export {scaleOrdinal} from "d3-scale";
 export {default as pipe} from "./src/pipe";
 export {default as assign} from "./src/assign";
 export {default as includes} from "./src/includes";
