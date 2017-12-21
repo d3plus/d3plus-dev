d1b9d194b41cc425afa6597bf17030092c1c38e0
Dave Landry <landry.dave@gmail.com>
2017-12-21 14:25:41 -0500

    adds module transpiling to enable tree-shaking

--- bin/build.js
+++ bin/build.js
@@ -4,7 +4,7 @@
     @module d3plus-build
     @summary Compiles all files for distribution.
     @desc This script will compile 2 builds, one with all dependencies includes (full) and one with only the core code. Next, each of those builds is minified using uglifyjs. Finally, all those builds, along with the LICENSE and README, are compressed into a .zip file.
-**/
+*/
 
 const log = require("./log")("build compile"),
       rollup = require("./rollup"),
@@ -12,38 +12,49 @@ const log = require("./log")("build compile"),
       {name} = JSON.parse(shell.cat("package.json"));
 
 shell.config.silent = true;
+
+/**
+    @summary Handles the results of the shell.exec callback function.
+    @private
+*/
 function kill(code, stdout) {
   log.fail();
   shell.echo(stdout);
   shell.exit(code);
 }
 
-rollup().then(() => {
-  rollup({deps: true}).then(() => {
+log.timer("transpiling ES6 for modules");
+shell.exec("buble -i src --no modules -m -o es", (code, stdout) => {
+  if (code) kill(code, stdout);
 
-    log.timer("uglifying builds");
-    shell.exec(`uglifyjs build/${name}.js -m --comments -o build/${name}.min.js`, (code, stdout) => {
-      if (code) kill(code, stdout);
+  rollup().then(() => {
+    rollup({deps: true}).then(() => {
 
-      shell.exec(`uglifyjs build/${name}.full.js -m --comments -o build/${name}.full.min.js`, (code, stdout) => {
+      log.timer("uglifying builds");
+      shell.exec(`uglifyjs build/${name}.js -m --comments -o build/${name}.min.js`, (code, stdout) => {
         if (code) kill(code, stdout);
 
-        log.timer("creating .zip distribution");
-        const files = ["LICENSE", "README.md",
-          `build/${name}.js`, `build/${name}.min.js`,
-          `build/${name}.full.js`, `build/${name}.full.min.js`
-        ];
-        shell.exec(`rm -f build/${name}.zip && zip -j -q build/${name}.zip -- ${files.join(" ")}`, (code, stdout) => {
+        shell.exec(`uglifyjs build/${name}.full.js -m --comments -o build/${name}.full.min.js`, (code, stdout) => {
           if (code) kill(code, stdout);
 
-          log.exit();
-          shell.exit(0);
+          log.timer("creating .zip distribution");
+          const files = ["LICENSE", "README.md",
+            `build/${name}.js`, `build/${name}.min.js`,
+            `build/${name}.full.js`, `build/${name}.full.min.js`
+          ];
+          shell.exec(`rm -f build/${name}.zip && zip -j -q build/${name}.zip -- ${files.join(" ")}`, (code, stdout) => {
+            if (code) kill(code, stdout);
+
+            log.exit();
+            shell.exit(0);
+
+          });
 
         });
 
       });
 
     });
-
   });
+
 });
