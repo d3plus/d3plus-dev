f928dc3db39031ad3f17bd0e0299f39759006781
Dave Landry <landry.dave@gmail.com>
2019-02-12 14:31:28 -0500

    fixes innerHTML polyfill

--- bin/polyfill/innerHTML.js
+++ bin/polyfill/innerHTML.js
@@ -1,71 +1,69 @@
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/startsWith
 
-module.exports = `if (!SVGElement.prototype.innerHTML) {
-  (function () {
-    var serializeXML = function (node, output) {
-      var nodeType = node.nodeType;
-      if (nodeType === 3) {
-        output.push(node.textContent.replace(/&/, '&amp;').replace(/</, '&lt;').replace('>', '&gt;'));
-      } else if (nodeType === 1) {
-        output.push('<', node.tagName);
-        if (node.hasAttributes()) {
-          [].forEach.call(node.attributes, function(attrNode){
-            output.push(' ', attrNode.item.name, '=\\'', attrNode.item.value, '\\'');
-          })
-        }
-        if (node.hasChildNodes()) {
-          output.push('>');
-          [].forEach.call(node.childNodes, function(childNode){
-            serializeXML(childNode, output);
-          })
-          output.push('</', node.tagName, '>');
-        } else {
-          output.push('/>');
-        }
-      } else if (nodeType == 8) {
-        output.push('<!--', node.nodeValue, '-->');
+module.exports = `(function () {
+  var serializeXML = function (node, output) {
+    var nodeType = node.nodeType;
+    if (nodeType === 3) {
+      output.push(node.textContent.replace(/&/, '&amp;').replace(/</, '&lt;').replace('>', '&gt;'));
+    } else if (nodeType === 1) {
+      output.push('<', node.tagName);
+      if (node.hasAttributes()) {
+        [].forEach.call(node.attributes, function(attrNode){
+          output.push(' ', attrNode.item.name, '=\\'', attrNode.item.value, '\\'');
+        })
       }
-    }
-
-    Object.defineProperty(SVGElement.prototype, 'innerHTML', {
-      get: function () {
-        var output = [];
-        var childNode = this.firstChild;
-        while (childNode) {
+      if (node.hasChildNodes()) {
+        output.push('>');
+        [].forEach.call(node.childNodes, function(childNode){
           serializeXML(childNode, output);
+        })
+        output.push('</', node.tagName, '>');
+      } else {
+        output.push('/>');
+      }
+    } else if (nodeType == 8) {
+      output.push('<!--', node.nodeValue, '-->');
+    }
+  }
+
+  Object.defineProperty(SVGElement.prototype, 'innerHTML', {
+    get: function () {
+      var output = [];
+      var childNode = this.firstChild;
+      while (childNode) {
+        serializeXML(childNode, output);
+        childNode = childNode.nextSibling;
+      }
+      return output.join('');
+    },
+    set: function (markupText) {
+      while (this.firstChild) {
+        this.removeChild(this.firstChild);
+      }
+
+      try {
+        var dXML = new DOMParser();
+        dXML.async = false;
+
+        var sXML = '<svg xmlns=\\'http://www.w3.org/2000/svg\\' xmlns:xlink=\\'http://www.w3.org/1999/xlink\\'>' + markupText + '</svg>';
+        var svgDocElement = dXML.parseFromString(sXML, 'text/xml').documentElement;
+
+        var childNode = svgDocElement.firstChild;
+        while (childNode) {
+          this.appendChild(this.ownerDocument.importNode(childNode, true));
           childNode = childNode.nextSibling;
         }
-        return output.join('');
-      },
-      set: function (markupText) {
-        while (this.firstChild) {
-          this.removeChild(this.firstChild);
-        }
+      } catch (e) {};
+    }
+  });
 
-        try {
-          var dXML = new DOMParser();
-          dXML.async = false;
+  Object.defineProperty(SVGElement.prototype, 'innerSVG', {
+    get: function () {
+      return this.innerHTML;
+    },
+    set: function (markup) {
+      this.innerHTML = markup;
+    }
+  });
 
-          var sXML = '<svg xmlns=\\'http://www.w3.org/2000/svg\\' xmlns:xlink=\\'http://www.w3.org/1999/xlink\\'>' + markupText + '</svg>';
-          var svgDocElement = dXML.parseFromString(sXML, 'text/xml').documentElement;
-
-          var childNode = svgDocElement.firstChild;
-          while (childNode) {
-            this.appendChild(this.ownerDocument.importNode(childNode, true));
-            childNode = childNode.nextSibling;
-          }
-        } catch (e) {};
-      }
-    });
-
-    Object.defineProperty(SVGElement.prototype, 'innerSVG', {
-      get: function () {
-        return this.innerHTML;
-      },
-      set: function (markup) {
-        this.innerHTML = markup;
-      }
-    });
-
-  })();
-}`;
+})();`;
