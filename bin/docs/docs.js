adb13f2d921f6df1eab4b43943ff739b467a6252
Dave Landry <landry.dave@gmail.com>
2021-12-16 13:11:29 -0500

    removes examples script and gh-pages references (closes #128)

--- bin/docs/docs.js
+++ bin/docs/docs.js
@@ -6,8 +6,7 @@
     @desc Generates the READEME.md documentation based on the JSDoc comments in the codebase. This script will overwrite README.md, but will not do any interaction with Github (commit, push, etc).
 */
 
-const jsdoc2md = require("jsdoc-to-markdown"),
-      log = require("../log")("documentation"),
+const log = require("../log")("documentation"),
       shell = require("shelljs");
 
 shell.config.silent = true;
@@ -15,69 +14,6 @@ const {description, name, version} = JSON.parse(shell.cat("package.json"));
 
 const versions = version.split(".").map(Number);
 const major = versions[0];
-
-/**
-    @function getVar
-    @summary Parses out variables from the top of an example file.
-    @param {String} contents File to parse.
-    @param {String} key Variable name to look for.
-    @param {Number|String} def Default fallback value to use if not found.
-    @param {Boolean} [num = true] Whether or not the value should be coerced into a Number.
-    @private
-*/
-function getVar(contents, key, def = 0, num = true) {
-  const r = new RegExp(`\\[${key}\\]: ([0-9]+)`, "g").exec(contents);
-  return r ? num ? parseFloat(r[1], 10) : r[1] : def;
-}
-
-log.timer("detecting upcoming releases");
-
-let examples = "";
-
-if (shell.test("-d", "example")) {
-
-  log.timer("analyzing example directory");
-
-  let header = false;
-
-  if (shell.test("-f", "example/getting-started.md")) {
-
-    const contents = shell.cat("example/getting-started.md"),
-          width = getVar(contents, "width", 990);
-
-    const link = `https://d3plus.org/examples/${name}/getting-started/`;
-    header = `${contents}
-
-[<kbd><img src="/example/getting-started.png" width="${width}px" /></kbd>](${link})
-
-[Click here](${link}) to view this example live on the web.
-
-
-`;
-    header = header.replace(/\n# |^# /g, "\n## ");
-
-  }
-
-  const now = new Date().getTime(), week = 1000 * 60 * 60 * 24 * 7;
-  const addl = shell.ls("-l", "example/*.md")
-    .filter(f => !f.name.includes("getting-started.md"))
-    .sort((a, b) => b.birthtime - a.birthtime)
-    .map(file => {
-      const re = new RegExp("# (.*?)\\n", "g");
-      const h1 = re.exec(shell.cat(file.name));
-      const title = h1 ? h1[1] : file.name.replace("example/", "").replace(".md", "");
-      const url = `http://d3plus.org/${file.name.replace("example", `examples/${name}`).replace(".md", "/")}`;
-      const suffix = now - file.ctime < week ? "<sup> ***New***</sup>"
-        : now - file.mtime < week ? "<sup> ***Updated***</sup>" : "";
-      return ` * [${title}](${url})${suffix}`;
-    });
-
-  if (!header && addl.length) header = "## Examples\n";
-  else if (addl.length) header = `${header}### More Examples\n`;
-
-  if (header) examples = `${header}\n${addl.join("\n")}\n`;
-
-}
 
 log.timer("writing JSDOC comments to README.md");
 
@@ -103,7 +39,6 @@ If you use NPM, \`npm install ${name}\`. Otherwise, download the [latest release
 </script>
 \`\`\`
 
-${examples}
 ## API Reference
 
 ${toc}
@@ -145,45 +80,8 @@ shell.exec(`jsdoc2md '+(bin|src)/**/*.+(js|jsx)' ${jsdocConfig} -t ${ template }
 
   shell.exec("git add README.md && git commit -m \"updates documentation\" && git push", () => {
 
-    if (shell.test("-d", "../d3plus-website") && !["react", "website", "workshop"].includes(name.split("-")[1])) {
-
-      log.timer("compiling website documentation");
-      const templateData = jsdoc2md.getTemplateDataSync({files: "+(bin|src)/**/*.+(js|jsx)"});
-
-      templateData.forEach(data => {
-        const {access, kind, memberof, name} = data;
-        if (!["constructor", "external", "module", "typedef"].includes(kind) && memberof === undefined && access !== "private") {
-          const docTemplate = `{{#${kind} name="${name}"}}{{>website}}{{/${kind}}}`;
-          const output = jsdoc2md.renderSync({
-            data: templateData,
-            helper: `${ __dirname }/helpers.js`,
-            partial: `${ __dirname }/partials/*.hbs`,
-            separators: true,
-            template: docTemplate
-          }).replace(/https:\/\/github\.com\/d3plus\/d3plus-[a-z]+#/g, "#");
-          shell.mkdir("-p", `../d3plus-website/_docs/${kind}`);
-          new shell.ShellString(`---
-name: ${name}
-kind: ${kind}
----
-
-${output}`).to(`../d3plus-website/_docs/${kind}/${name}.md`);
-        }
-      });
-
-      shell.cd("../d3plus-website");
-      shell.exec(`git add _docs/* && git commit -m \"updates ${name} documentation\" && git push`, () => {
-
-        log.exit();
-        shell.exit(0);
-
-      });
-
-    }
-    else {
-      log.exit();
-      shell.exit(0);
-    }
+    log.exit();
+    shell.exit(0);
 
   });
 
