e65703021033d0a1a772cb38644cecce59e4aa82
Dave Landry <landry.dave@gmail.com>
2023-01-04 17:19:37 -0500

    adds XVFB action to Node CI test workflow

--- bin/env/_NodeCI.js
+++ bin/env/_NodeCI.js
@@ -7,29 +7,36 @@ module.exports = log => {
   shell.mkdir("-p", ".github/workflows");
   new shell.ShellString(`name: Node.js CI
 
-on:
-  push:
-    branches: [ main ]
-  pull_request:
-    branches: [ main ]
+on: [push]
 
 jobs:
   build:
 
-    runs-on: ubuntu-latest
-
     strategy:
       matrix:
-        node-version: [12.x]
+        os: [macos-latest, ubuntu-latest, windows-latest]
 
     steps:
-      - uses: actions/checkout@v3
+      - name: Checkout
+        uses: actions/checkout@v3
+      - name: Setup xvfb (Linux)
+        if: runner.os == 'Linux'
+        run: |
+          sudo apt-get install -y xvfb libxkbcommon-x11-0 libxcb-icccm4 libxcb-image0 libxcb-keysyms1 libxcb-randr0 libxcb-render-util0 libxcb-xinerama0 libxcb-xinput0 libxcb-xfixes0
+          # start xvfb in the background
+          sudo /usr/bin/Xvfb $DISPLAY -screen 0 1280x1024x24 &
       - name: Use Node.js \${{ matrix.node-version }}
         uses: actions/setup-node@v3
         with:
           node-version: \${{ matrix.node-version }}
-      - run: npm ci
-      - run: npm test
+      - name: Install Dependencies
+        run: npm ci
+      - name: Link d3plus-dev Scripts
+        run: npm link
+      - name: Run Headless Tests
+        uses: GabrielBB/xvfb-action@v1
+        with:
+          run: npm test
 `).to(".github/workflows/NodeCI.yml");
 
 };
