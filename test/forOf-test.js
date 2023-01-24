987704b96f0bade730cec6de293a40fcf11d3e50
Dave Landry <landry.dave@gmail.com>
2023-01-24 18:47:34 -0500

    polygills missing global keys in JSDOM

--- test/forOf-test.js
+++ test/forOf-test.js
@@ -6,6 +6,6 @@ it("for-of", () => {
 
   const result = forOf(10);
   assert.strictEqual(result.arr[9], 18, "for-of transpiling");
-  assert.strictEqual(result.size, 3, "d3 es6 transpiling");
+  assert.strictEqual(result.size, 4, "d3 es6 transpiling");
 
 });
